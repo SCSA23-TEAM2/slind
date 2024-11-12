d655e24b5a8915e30d083ae5df4cf10e66e91fb1
aTeNaNt <panzer1209@naver.com>
2024-11-12 17:28:47 +0900

    feat Add GeneralBoard Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
+import GeneralBoard from "./components/GeneralBoard";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Login />
-      {/* <Header />
-      <Nav /> */}
+      {/* <Login /> */}
+      <Header />
+      <Nav />
+      <GeneralBoard />
     </>
   );
 }
---
+++ frontend/src/components/GeneralBoard.jsx
@@ -0,0 +1,19 @@
+import "./css/GeneralBoard.css";
+
+const GeneralBoard = () => {
+  return (
+    <div className="generalBoard-background">
+      <div className="generalBoard-wrapper">
+        <div className="board-header">
+          <h1>wow</h1>
+        </div>
+        <div className="board-item-wrapper">
+          <ul></ul>
+        </div>
+        <div className="board-bottom-wrapper"></div>
+      </div>
+    </div>
+  );
+};
+
+export default GeneralBoard;
