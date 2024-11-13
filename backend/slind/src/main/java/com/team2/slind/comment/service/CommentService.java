d07815e2ce0fea68e26320c4871976acf8528e39
Kim Junha <kimjunha1575@gmail.com>
2024-11-13 11:18:19 +0900

    feat: Implement comment list API  - User part is not implemented yet (Always return false for user related variables)

--- backend/slind/src/main/java/com/team2/slind/comment/service/CommentService.java
+++ backend/slind/src/main/java/com/team2/slind/comment/service/CommentService.java
@@ -1,12 +1,14 @@
 package com.team2.slind.comment.service;
 
-import com.team2.slind.comment.dto.request.CommentListRequest;
 import com.team2.slind.comment.dto.response.CommentListResponse;
+import com.team2.slind.comment.dto.response.CommentResponse;
 import com.team2.slind.comment.mapper.CommentMapper;
 import com.team2.slind.comment.mapper.CommentReactionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +16,17 @@ public class CommentService {
     private final CommentMapper commentMapper;
     private final CommentReactionMapper commentReactionMapper;
 
-    public ResponseEntity<CommentListResponse> getCommentList(Long articlePk, Long lastCommentPk) {
+    public ResponseEntity<CommentListResponse> getCommentList(Long articlePk, Long lastCommentPk, int fetchCount) {
         CommentListResponse response = new CommentListResponse();
+        List<CommentResponse> comments = commentMapper.getCommentList(articlePk, lastCommentPk, fetchCount);
+        response.setHasNext(comments.size() > fetchCount);
+        response.setList(comments);
+        // TODO: Implement user part
+        for (CommentResponse commentResponse : comments) {
+            commentResponse.setIsLike(false);
+            commentResponse.setIsDislike(false);
+            commentResponse.setIsMine(false);
+        }
         return ResponseEntity.ok(response);
     }
 }
