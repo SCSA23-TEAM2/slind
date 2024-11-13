07ce7c11ca6ee43298817868814f36fd4541641c
Kim Junha <kimjunha1575@gmail.com>
2024-11-13 17:06:38 +0900

    fix: Change mapper return type

--- backend/slind/src/main/java/com/team2/slind/comment/mapper/CommentMapper.java
+++ backend/slind/src/main/java/com/team2/slind/comment/mapper/CommentMapper.java
@@ -1,15 +1,19 @@
 package com.team2.slind.comment.mapper;
 
 import com.team2.slind.comment.dto.response.CommentResponse;
+import com.team2.slind.comment.vo.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentMapper {
     List<CommentResponse> getCommentList(Long articlePk, Long lastCommentPk, int fetchCount);
     List<CommentResponse> getBestCommentList(Long articlePk, int fetchCount);
     List<CommentResponse> getRecommentList(Long originateComment, Long lastCommentPk, int fetchCount);
-    Integer createComment(Long memberPk, Long articlePk, String commentContent);
-    Integer updateComment(Long memberPk, Long commentPk, String commentContent);
-    Integer deleteComment(Long memberPk, Long commentPk, String deleteMessage);
-    Integer createRecomment(Long memberPk, Long originateComment, String commentContent);
+    Long createComment(Long memberPk, Long articlePk, String commentContent);
+    Long updateComment(Long memberPk, Long commentPk, String commentContent);
+    Long deleteComment(Long memberPk, Long commentPk, String deleteMessage);
+    Long createRecomment(Long memberPk, Long originateComment, String commentContent);
+    Long updateRecomment(Long memberPk, Long commentPk, String commentContent);
+    Optional<Comment> findByPk(Long commentPk);
 }
