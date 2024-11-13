b144889c9ce7de791ac4afc9f6b3ba5828ad341f
Kim Junha <kimjunha1575@gmail.com>
2024-11-13 17:07:15 +0900

    chore: Add exception messages

--- backend/slind/src/main/java/com/team2/slind/common/exception/UnauthorizedException.java
+++ backend/slind/src/main/java/com/team2/slind/common/exception/UnauthorizedException.java
@@ -4,6 +4,8 @@ public class UnauthorizedException extends RuntimeException {
     public static final String UNAUTHORIZED_DELETE_BOARD = "게시판 삭제 권한이 없는 계정입니다.";
     public static final String UNAUTHORIZED_UPDATE_ARTICLE = "게시글 수정 권한이 없는 계정입니다.";
     public static final String UNAUTHORIZED_DELETE_ARTICLE = "게시글 삭제 권한이 없는 계정입니다.";
+    public static final String UNAUTHORIZED_UPDATE_COMMENT = "댓글 수정 권한이 없는 계정입니다.";
+    public static final String UNAUTHORIZED_DELETE_COMMENT = "댓글 삭제 권한이 없는 계정입니다.";
 
     public UnauthorizedException(String message) {
         super(message);
