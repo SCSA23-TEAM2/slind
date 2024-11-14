a7f4500bbadcae8e107dff08d288e982f15d2509
psy <qkrtudwkd@gmail.com>
2024-11-14 17:30:35 +0900

    Add Jwt Configuration

---
+++ backend/slind/src/main/java/com/team2/slind/security/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,7 @@
+//package com.team2.slind.security.jwt.filter;
+//
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//    private static final String NO_CHECK_URL = ""
+//}
---
+++ backend/slind/src/main/java/com/team2/slind/security/jwt/service/JwtService.java
@@ -0,0 +1,101 @@
+package com.team2.slind.security.jwt.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+
+//@Service
+@RequiredArgsConstructor
+@Getter
+@Slf4j
+public class JwtService {
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${jwt.access.expiration}")
+    private Long accessTokenExpirationPeriod;
+
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshTokenExpirationPeriod;
+
+    @Value("${jwt.access.header}")
+    private String accessHeader;
+
+    @Value("${jwt.refresh.header}")
+    private String refreshHeader;
+
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String MEMBERID_CLAIM = "memberId";
+    private static final String BEARER = "Bearer ";
+
+    public String createAccessToken(String memberId){
+        Date now = new Date();
+        return JWT.create()
+                .withSubject(ACCESS_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(now.getTime() + accessTokenExpirationPeriod))
+                .withClaim(MEMBERID_CLAIM, memberId)
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    public String createRefreshToken(String memberId){
+        Date now = new Date();
+        return JWT.create()
+                .withSubject(REFRESH_TOKEN_SUBJECT)
+                .withExpiresAt(new Date(now.getTime() + refreshTokenExpirationPeriod))
+                .sign(Algorithm.HMAC512(secretKey));
+    }
+
+    public void sendAccessToken(HttpServletResponse response, String accessToken){
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.setHeader(accessHeader, accessToken);
+        log.info("Access Token : {}", accessToken);
+    }
+
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken){
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken);
+        setRefreshTokenHeader(response, refreshToken);
+        log.info("Access Token, Refresh Token 헤더 설정 완료");
+    }
+
+    public Optional<String> extractRefreshToken(HttpServletRequest request){
+        return Optional.ofNullable(request.getHeader(refreshHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+
+    public Optional<String> extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(accessHeader))
+                .filter(refreshToken -> refreshToken.startsWith(BEARER))
+                .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    }
+    public Optional<String> extractMemberId(String accessToken){
+        return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secretKey))
+                .build()
+                .verify(accessToken)
+                .getClaim(MEMBERID_CLAIM)
+                .asString());
+    }
+
+    public void setAccessTokenHeader(HttpServletResponse response, String accessToken){
+        response.setHeader(accessHeader, accessToken);
+    }
+
+    public void setRefreshTokenHeader(HttpServletResponse response, String refreshToken){
+        response.setHeader(refreshHeader, refreshToken);
+    }
+
+}
